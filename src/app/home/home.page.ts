4de5cf0d606f0d37f1cfbbf6075d9c9503072291
Carlos Sierra <casl02032001@gmail.com>
2022-07-25 21:28:10 -0500

    proyecto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slideOpt = {
+    initialSlide: 0, // Slide inicial
+    slidesPerView: 1, // Slide por vistas
+    centeredSlides: true, // Centrado de las slides
+    speed: 400 // Velocida de transicion de cada slide en milisegundos
+  }
+
   constructor() {}
 
 }
